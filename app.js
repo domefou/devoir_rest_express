e0af83b0a3c2aeaf6cf941b19b3d0e24dc10132c
Lecoultrejean <lecoultre.dev85@gmail.com>
2025-03-17 02:50:30 +0100

    ajout du port 3000

--- app.js
+++ app.js
@@ -113,6 +113,14 @@ app.use((req, res, next) => {
     res.status(404).json({ name: 'API', version: '1.0', status: 404, message: 'Not Found' });
 });
 
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
 module.exports = app;
 
 
